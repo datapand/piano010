9347b387722a3074176d6269d406f5817d79de1c
Bahadir Kasap <bkasap@gmail.com>
2024-11-16 10:03:46 +0100

    updated cookie consent and google analytics

---
+++ assets/scripts/cookie-consent.js
@@ -0,0 +1,15 @@
+window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#000"
+      },
+      "button": {
+        "background": "#f1d600"
+      }
+    },
+    "type": "opt-in",
+    "content": {
+      "message": "This website uses cookies to ensure you get the best experience here. Once you accept, you can always opt-out under privacy policy page.",
+      "href": "/privacy-policy"
+    },
+  });
---
+++ pages/en/thanks.md
@@ -0,0 +1,20 @@
+---
+title: Thanks!
+menu_title: Thanks!
+feature_text: |
+  Thanks for getting in touch!
+layout: page
+lang: en
+permalink: /thanks/
+page_id: thanks
+indexing: false
+sitemap: false
+---
+
+<a href="javascript:history.back()">Go Back</a>
+
+Thanks for getting in touch!
+
+I will respond as soon as poosible, 
+
+until then...
